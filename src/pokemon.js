670b30d9bfc3ea131f8d1978ef49b60a7c289709
John Mitchell <johnnn.mitchell@gmail.com>
2026-03-10 14:01:19 -0700

    firebase sa

--- src/pokemon.js
+++ src/pokemon.js
@@ -1,16 +1,15 @@
 const { EmbedBuilder } = require('discord.js');
-const { initializeApp } = require("firebase/app");
-const { getFirestore, collection, doc, setDoc, getDoc } = require("firebase/firestore");
+const { getFirestore, doc, setDoc, getDoc } = require("firebase/firestore");
+const admin = require('firebase-admin');
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-const app = initializeApp(firebaseConfig);
+const jsonString = Buffer.from(
+    process.env.FIREBASE_CREDENTIALS_BASE64,
+    'base64'
+).toString('utf8');
+const credential = admin.credential.cert(JSON.parse(jsonString));
+const app = admin.initializeApp({
+    credential,
+});
 const db = getFirestore(app);
 
 const POKEMON_CATCH_RATE = 0.25;
